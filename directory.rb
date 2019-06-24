4a3771a0af98dcce0b444e91842f6f1cf0cb6dfa
Ted <t.scanlan92@gmail.com>
2019-06-24 23:26:03 +0100

    names added

---
+++ directory.rb
@@ -0,0 +1,8 @@
+puts "The students of Villains Academy"
+puts "-------------------"
+puts "Ted Scanlan"
+puts "Jay Bacon"
+puts "Tom Ivey"
+puts "Louie Findlater"
+puts "Billy Brown"
+puts "Darth Vader"
